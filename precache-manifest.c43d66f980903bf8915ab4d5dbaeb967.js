810b1584ffad7e1bd312d2eda2d79d1577632056
Dave Pernitz <pernitz@uwalumni.com>
2020-05-26 07:24:18 -0600

    Updates

---
+++ precache-manifest.c43d66f980903bf8915ab4d5dbaeb967.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ee7b56f54b7a83ea045ecb8e4d63b6",
+    "url": "/dave-pernitz-portfolio/index.html"
+  },
+  {
+    "revision": "abd98854d9a8b8b1fb09",
+    "url": "/dave-pernitz-portfolio/static/css/main.64a5e76c.chunk.css"
+  },
+  {
+    "revision": "f4fa8ef32545a34d90ef",
+    "url": "/dave-pernitz-portfolio/static/js/2.a2d8c822.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dave-pernitz-portfolio/static/js/2.a2d8c822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd98854d9a8b8b1fb09",
+    "url": "/dave-pernitz-portfolio/static/js/main.a769c0d8.chunk.js"
+  },
+  {
+    "revision": "264e7346594bb03134a1",
+    "url": "/dave-pernitz-portfolio/static/js/runtime-main.0133f978.js"
+  }
+]);
