10fefe53471756cfa8a0466de0d0986f15899b01
Dave Pernitz <pernitz@uwalumni.com>
2020-05-27 09:37:39 -0600

    Updates

--- precache-manifest.638ee1b7d8748c2bbca95f03dac515a9.js
+++ precache-manifest.638ee1b7d8748c2bbca95f03dac515a9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "387d379969e7a3019ea76b27e55ca2d5",
+    "revision": "13d47713987dcd90b6b68115917526ef",
     "url": "/dave-pernitz-portfolio/index.html"
   },
   {
-    "revision": "a7e1bfded2e756078c1e",
+    "revision": "07061299c53ad1dd24f1",
     "url": "/dave-pernitz-portfolio/static/css/main.64a5e76c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/dave-pernitz-portfolio/static/js/2.a2d8c822.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a7e1bfded2e756078c1e",
-    "url": "/dave-pernitz-portfolio/static/js/main.ab9901f6.chunk.js"
+    "revision": "07061299c53ad1dd24f1",
+    "url": "/dave-pernitz-portfolio/static/js/main.6b01dd06.chunk.js"
   },
   {
     "revision": "264e7346594bb03134a1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dave-pernitz-portfolio/precache-manifest.a2d334daa9943688c5befaf9b1a6c623.js"
+  "/dave-pernitz-portfolio/precache-manifest.638ee1b7d8748c2bbca95f03dac515a9.js"
 );
 
 self.addEventListener('message', (event) => {
